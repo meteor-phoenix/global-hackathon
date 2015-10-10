13ced3164799826f3e45fc23544e958356f752cd
Ivan Montiel <idmontie@asu.edu>
2015-10-10 15:09:39 -0700

    Stubs for github job

---
+++ src/server/git/github-job.js
@@ -0,0 +1,11 @@
+/**
+ * Github Job
+ *
+ * For each github organization, ask for the latest issues
+ * and then run a diff between the changes. For those changes,
+ * run any necessary events.
+ */
+Meteor.setInterval( function GithubJob () {
+  
+
+}, 60 * 1000 /* 1 minute */ );
