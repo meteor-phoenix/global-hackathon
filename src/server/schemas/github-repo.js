1144f808fbd24e6800536062ad17993ee0552026
standard <standard@localhost.localdomain>
2015-10-10 16:43:49 -0700

    Added 2 schemas and conected them to their collections using collection2

---
+++ src/server/schemas/github-repo.js
@@ -0,0 +1,24 @@
+GithubRepo = new Mongo.Collection("GithubRepo");
+
+Schemas = {}
+
+Schemas.Github = new SimpleSchema({
+  orgName: {
+    type: String,
+    label: "Organization Name"
+    max:200
+  },
+  repoName: {
+    type: String,
+    label: "Repository Name"
+    max:200
+  },
+  lastPollTimestamp: {
+    type:Boolean
+    label: "Time Events Last Polled" 
+  }
+})
+
+GithubRepo.attachSchema(Shemas.Github)
+
+obj = {orgName: "meteor-phoenix", repoName: "global-hackathon"};
