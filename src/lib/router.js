82d198aa432c01bef5b32614d68e3716f94220e7
William <hello@bigwilliam.com>
2015-10-10 18:57:51 -0700

    added default route from kadira docs

--- src/lib/router.js
+++ src/lib/router.js
@@ -3,8 +3,21 @@ FlowRouter.route('/', {
     action: function(params, queryParams) {
       console.log("Params:", params);
       console.log("Query Params:", queryParams);
-      ReactLayout.render("MainLayout");
+      ReactLayout.render(MainLayout);
     },
 
     name: "Main Layout" // optional
 });
+
+
+// FlowRouter.route("/", {
+//   subscriptions: function() {
+//     var selector = {category: {$ne: "private"}};
+//     this.register('posts', Meteor.subscribe('posts', selector));
+//   },
+//   action: function() {
+//     ReactLayout.render(BlogLayout, {
+//       content: <PostList />
+//     });
+//   }
+// });
