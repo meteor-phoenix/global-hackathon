32c2bc72bb6a4b3b67a05fecfa0fcd766c6ae699
Ivan Montiel <idmontie@asu.edu>
2015-10-17 15:05:12 -0700

    Add loading screens

--- src/client/views/layouts/main-layout.jsx
+++ src/client/views/layouts/main-layout.jsx
@@ -2,13 +2,27 @@ MainLayout = React.createClass({
   render() {
     return <div>
       <header>
+        <a className="sr-only" href="#top" name="top" id="top">&nbsp;</a>
         <MenuLayout />
       </header>
       <main>
         {this.props.content}
       </main>
       <footer>
-        Made by Meteor Phoenix <a href="http://github.com/meteor-phoenix/global-hackathon">View on Github</a>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+
+              <ul className="list-unstyled">
+                <li className="pull-right"><a href="#top">Back to top</a></li>
+                <li><a href="/about">About</a></li>
+              </ul>
+              <p>Made by Meteor Phoenix.</p>
+              <p><a href="http://github.com/meteor-phoenix/global-hackathon">View on Github</a>.</p>
+              <p>Code released under the <a href="http://github.com/meteor-phoenix/global-hackathon/blob/master/LICENSE">MIT License</a>.</p>
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
   }
--- src/client/views/react-components/app-loading-component.jsx
+++ src/client/views/react-components/app-loading-component.jsx
@@ -1,7 +1,32 @@
 AppLoadingComponent = React.createClass({
   render() {
-    return <div>
-      Loading
+    var fun = [
+        "Calculating levels...",
+        "Awarding medals of honor...",
+        "Hunting for unicorns...",
+        "Crunching skill levels...",
+        "Finding quests...",
+      ];
+    fun.sort(function () { return 0.5 - Math.random(); });
+
+    var message = fun[0];
+
+    var text = (
+      <div id="loader-pitch">
+        <h1>Loading</h1>
+        <h2>{message}</h2>
+      </div>
+    );
+    if (this.props.noText) {
+      text = '';
+    }
+
+    return <div className="loading">
+      <div className="spinner">
+        <div className="cube1"></div>
+        <div className="cube2"></div>
+      </div>
+      {text}
     </div>
   }
 });
